bd91e78e79836dbcbe3c60c6b08087e95b7d3047
antitahil <antitahil@gmail.com>
2025-06-26 04:41:21 -0400

    Create 0-helloworld.c

---
+++ 0-helloworld.c
@@ -0,0 +1,11 @@
+/* This prints the line "Programming is like building a multilingual puzzle" 
+* followed by a new line.
+* 
+* Return: 0
+*/
+int main(void)
+{
+  printf("Programming is like building a multilingual puzzle\n");
+
+  return (0);
+}
